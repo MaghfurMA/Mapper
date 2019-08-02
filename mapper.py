60ff558e3481b7ad14f0f7dd3b056f153901656e
Moh. Alex Maghfur <39029449+MaghfurMA@users.noreply.github.com>
2019-08-02 16:43:02 +0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,23 +1,11 @@
-#!/usr/bin/env python
-
-
 import sys
+import re
 
 for line in sys.stdin:
+    line = " ".join(re.findall("[a-zA-Z]+", line))
+    line=line.lower()
     line = line.strip()
     words = line.split()
     if len(line) > 1:
         for word in words:
             print ('%s\t%d' % (word, 1))
-            
-            
-for line in sys.stdin:
-    words = line.split()
-        for word in words:
-            word = "".join(i for i in word if ord(i)==32 or 47<ord(i)<58 or 64<ord(i)<91 or 96<ord(i)<123)
-          
-          
-for line in sys.stdin:
-    words = line.split()
-        for word in words:
-            word = word.lower()
